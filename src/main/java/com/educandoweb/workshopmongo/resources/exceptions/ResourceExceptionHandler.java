a923a3d3a488adce6c9c9ad244c265402a0ec066
Tarcísio Mesquita <tarcisiomesquitafilho@gmail.com>
2021-05-04 13:15:33 -0300

    Getting a user by id

---
+++ src/main/java/com/educandoweb/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.educandoweb.workshopmongo.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error , e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
